31bbc5839e656a4056eb4503e636e9779b349815
Nathan Lambertson <lambertones@gmail.com>
2023-12-01 19:05:26 -0600

    Move show test from index_spec to show_spec

--- spec/features/musicians/index_spec.rb
+++ spec/features/musicians/index_spec.rb
@@ -51,16 +51,6 @@ RSpec.describe 'Musicians Index', type: :feature do
     end
   end
 
-  it "shows count of musician's synths when visiting musician's #show page", type: :feature do
-    coil = Musician.create!(name: "Coil")
-    synth1 = coil.synthesizers.create!(brand: "Moog", name: "Mother32")
-    synth2 = coil.synthesizers.create!(brand: "Roland", name: "M800")
-    synth3 = coil.synthesizers.create!(brand: "Moog", name: "Subp")
-
-    visit "/musicians/#{coil.id}"
-    expect(page).to have_content(coil.synthesizers.count)
-  end
-
   it "can be visited from any page on the site", type: :feature do
     vangelis = Musician.create!(name: "Vangelis", age: 79, genre: "Film", active: false)
     synth1 = vangelis.synthesizers.create!(brand: "Roland", name: "Juno-6", year: 1982, engine: "Analog", value: 2000, voice_count: 6, vintage:true)
--- spec/features/musicians/show_spec.rb
+++ spec/features/musicians/show_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Musicians #show', type: :feature do
+
+  it "shows count of musician's synths when visiting musician's #show page", type: :feature do
+    coil = Musician.create!(name: "Coil")
+    synth1 = coil.synthesizers.create!(brand: "Moog", name: "Mother32")
+    synth2 = coil.synthesizers.create!(brand: "Roland", name: "M800")
+    synth3 = coil.synthesizers.create!(brand: "Moog", name: "Subp")
+
+    visit "/musicians/#{coil.id}"
+    expect(page).to have_content(coil.synthesizers.count)
+  end
+
   it "has a link to that musician's synthesizers page" do
     vangelis = Musician.create!(name: "Vangelis", age: 79, genre: "Film", active: false)
     synth1 = vangelis.synthesizers.create!(brand: "Roland", name: "Juno-6", year: 1982, engine: "Analog", value: 2000, voice_count: 6, vintage:true)
